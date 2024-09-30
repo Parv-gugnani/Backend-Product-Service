881e9641ad7e5691b59eaf99b2c1e02168d67ed6
Parv-gugnani <9765.parv@gmail.com>
2024-09-30 13:25:04 +0530

    start

---
+++ App.js
@@ -0,0 +1,28 @@
+const express = require("express"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  bodyParser = require("body-parser"),
+  LocalStrategy = require("passport-local"),
+  passportLocalMongoose = require("passport-local-mongoose");
+const User = require("./model/User");
+
+let app = express();
+
+mongoose.connect("mongodb://localhost/27017");
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  require("express-session")({
+    secret: "Rusty is a Dog",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
